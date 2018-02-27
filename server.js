1d968175a026660b8d6ae3388634f7395e6ee83c
Neeraj-Kashyap <31131883+Neeraj-Kashyap@users.noreply.github.com>
2018-02-27 19:21:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.sendFile('Article one will be written here');
+});
+
+
+app.get('/article-two', function (req, res) {
+  res.sendFile('Article two will be written here');
+});
+
+
+app.get('/article-three', function (req, res) {
+  res.sendFile('Article three will be written here');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
